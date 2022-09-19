c3f2ac91410a8b0fdca7b334368cd8c4d8841894
pipupi <qiushic@andrew.cmu.edu>
2022-09-19 11:18:50 -0400

    test

--- in-lab-examples/puinote-lab03/puinote-start/js/app.js
+++ in-lab-examples/puinote-lab03/puinote-start/js/app.js
@@ -2,7 +2,7 @@ console.log("start application");
 
 function updateElement() {
     console.log("running the updateElement function");
-    const noteImageElement = document.querySelector('.notcard-thumbnail')
+    const noteImageElement = document.querySelector('.notcard-thumbnail');
     noteImageElement.src = notecard.noteImageURL;
     const noteTitleElement = document.querySelector('.note-title');
     noteTitleElement.innerText = notecard.noteTitle;
@@ -13,7 +13,7 @@ function updateElement() {
 const notecard = {
     noteTitle:'This is the title of the note',
     noteBody: 'Here is the body of the note',
-    noteImageURL: 'assets/warhol-frog.png'
+    noteImageURL: 'assets/warhol-eagle.png'
 
 }
 
@@ -22,4 +22,13 @@ function submitNote() {
     const editorBodyElement = document.querySelector('#note-editor-body');
     notecard.noteTitle = editorTtileElement.value;
     notecard.noteBody = editorBodyElement.value;
+    updateElement();
 }
+
+const btnSubmit = document.querySelector('#btn-submit');
+//register onclick event: submitNote will get called on click
+btnSubmit.onclick  = submitNote;
+
+// call submitNote right when the page loads
+// assign its return value back to onclick
+// btnSubmit onclick = submitNote();
