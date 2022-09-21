0e9fc8052d88bf0407e490ee68b9bdf0ecdd2302
Alexandra Ion <tell.alexiiion@gmail.com>
2022-09-20 21:47:31 -0400

    created rolls.js

---
+++ homework-materials/HW4/rolls.js
@@ -0,0 +1,26 @@
+const rolls = {
+    "Original": {
+        "basePrice": 2.49,
+        "img": "original-cinnamon-roll.jpg"
+    },
+    "Apple": {
+        "basePrice": 3.49,
+        "imgURL": "apple-cinnamon-roll.jpg"
+    },
+    "Raisin": {
+        "basePrice": 2.99,
+        "imgURL": "raisin-cinnamon-roll.jpg"
+    },
+    "Walnut": {
+        "basePrice": 3.49,
+        "imgURL": "walnut-cinnamon-roll.jpg"
+    },
+    "Double-Chocolate": {
+        "basePrice": 3.99,
+        "imgURL": "double-chocolate-cinnamon-roll.jpg"
+    },
+    "Strawberry": {
+        "basePrice": 3.99,
+        "imgURL": "strawberry-cinnamon-roll.jpg"
+    }    
+};
